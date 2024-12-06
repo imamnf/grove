e2d742c485e5d2dec53c13a3e76620ea971ddc93
Imam Nur <imamnurfahudi@gmail.com>
2024-12-06 15:05:40 +0700

    feat: add useDate default value for locales

--- src/composables/useDate.ts
+++ src/composables/useDate.ts
@@ -1,12 +1,12 @@
 export function useDate() {
-  function dateShort(date: string | Date, format: string = 'DD/MM/YYYY'): string {
-    const formatedDate = useDateFormat(date, format, { locales: 'id-ID' })
+  function dateShort(date: string | Date, format: string = 'DD/MM/YYYY', locales: string = 'id-ID'): string {
+    const formatedDate = useDateFormat(date, format, { locales })
 
     return formatedDate.value
   }
 
-  function dateLong(date: string | Date, format: string = 'DD/MM/YYYY HH:mm:ss'): string {
-    const formatedDate = useDateFormat(date, format, { locales: 'id-ID' })
+  function dateLong(date: string | Date, format: string = 'DD/MM/YYYY HH:mm:ss', locales: string = 'id-ID'): string {
+    const formatedDate = useDateFormat(date, format, { locales })
 
     return formatedDate.value
   }
